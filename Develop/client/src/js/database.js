338948efe3ca5828a44b0c7abff361e02c513ac3
unknown <jessedraper2@gmail.com>
2022-01-25 23:21:20 -0700

    fix site now debug

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (body) => {
+  export const putDb = async (body) => {
   console.log("Post to the database");
 
   const JATEDb = await openDB("JATE", 1);
@@ -35,7 +35,7 @@ export const putDb = async (body) => {
   }
 
   // TODO: Add logic for a method that gets all the content from the database
-  export const getDb = async () => {
+   export const getDb = async () => {
     console.log("GET from the database");
 
     const JATEDb = await openDB("JATE", 1);
